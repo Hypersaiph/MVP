44fcd056465e6578e40f2137aa3a378e46da42c7
Hypersaiph <mygvs.mh@gmail.com>
2018-10-21 23:28:09 -0400

    just kidding... x2

--- app/src/main/java/ai/gvs/mvp/MainActivity.kt
+++ app/src/main/java/ai/gvs/mvp/MainActivity.kt
@@ -5,10 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    //my github :v
-    //my github x2 :v
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        //my github :v
+        //my github x2 :v
+        var a = 1
+    }
 }
